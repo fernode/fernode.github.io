397acf886a0335534a92d66430175795274ad7b4
fernode <fernandosilvadj@hotmail.com>
2019-03-14 14:09:34 -0300

    Adicionando um novo projeto

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,5 +1,5 @@
 class TypeWriter {
-	constructor(txtElement, words, wait = 3000) {
+	constructor(txtElement, words, wait = 2000) {
 		this.txtElement = txtElement;
 		this.words = words;
 		this.txt = '';
@@ -21,7 +21,7 @@ class TypeWriter {
 
 		this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-		let typeSpeed = 300;
+		let typeSpeed = 250;
 
 		if (this.isDeleting) {
 			typeSpeed /= 2;
